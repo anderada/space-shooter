a6f5f5748bc313ee0309a45b4ff14da496387c41
Adam Anderson <anderada@sheridancollege.ca>
2024-10-11 12:25:07 +0000

    added camera following

--- Assets/Scripts/Controllers/Player.cs
+++ Assets/Scripts/Controllers/Player.cs
@@ -10,17 +10,21 @@ public class Player : MonoBehaviour
     public Transform bombsTransform;
     public GameObject powerupPrefab;
     public int circlePoints = 7;
-    public float circleRadius = 2;
+    public float circleRadius = 2f;
     public int powerupPoints = 5;
-    public float powerupRadius = 3;
+    public float powerupRadius = 3f;
 
-    Vector2 velocity;
-    public float maxSpeed = 5;
-    public float accTime = 1;
-    public float decTime = 1;
+    public Vector2 velocity;
+    public float maxSpeed = 5f;
+    public float accTime = 1f;
+    public float decTime = 1f;
+    float accPerSec;
+    float decPerSec;
 
     void Start(){
         velocity = new Vector2(0,0);
+        accPerSec = maxSpeed / accTime;
+        decPerSec = -maxSpeed / decTime;
     }
 
     void Update()
@@ -30,35 +34,48 @@ public class Player : MonoBehaviour
         if(Input.GetKeyDown(KeyCode.P)){
             SpawnPowerups(powerupRadius, powerupPoints);
         }
+        pointTowardsMouse();
+    }
+
+    public void pointTowardsMouse(){
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector3 mouseDifference = mousePos - transform.position;
+        mouseDifference = mouseDifference.normalized;
+        Debug.DrawLine(transform.position, mousePos, Color.red);
+        float theta = Mathf.Atan2(mouseDifference.y, mouseDifference.x) * Mathf.Rad2Deg;
+        theta -= 90;
+        transform.eulerAngles = new Vector3(0,0,theta);
     }
 
     public void PlayerMovement(){
 
-        velocity.x += Input.GetAxis("Horizontal") * Time.deltaTime/accTime;
-        velocity.y += Input.GetAxis("Vertical") * Time.deltaTime / accTime;
+        velocity.x += Input.GetAxis("Horizontal") * accPerSec * Time.deltaTime;
+        velocity.y += Input.GetAxis("Vertical") * accPerSec * Time.deltaTime;
 
         if (Input.GetAxis("Vertical") == 0)
         {
-            if(velocity.y > 0)
-                velocity.y -= Time.deltaTime / decTime;
+            if(velocity.y > 0.00001f)
+                velocity.y += decPerSec * Time.deltaTime;
+            else if(velocity.y < -0.00001f)
+                velocity.y -= decPerSec * Time.deltaTime;
             else
-                velocity.y += Time.deltaTime / decTime;
-
+                velocity.y = 0f;
         }
 
         if (Input.GetAxis("Horizontal") == 0)
         {
-            if (velocity.x > 0)
-                velocity.x -= Time.deltaTime / decTime;
+            if(velocity.x > 0.00001f)
+                velocity.x += decPerSec * Time.deltaTime;
+            else if(velocity.x < -0.00001f)
+                velocity.x -= decPerSec * Time.deltaTime;
             else
-                velocity.x += Time.deltaTime / decTime;
-
+                velocity.x = 0f;
         }
 
         velocity.x = Mathf.Clamp(velocity.x, -maxSpeed, maxSpeed);
         velocity.y = Mathf.Clamp(velocity.y, -maxSpeed, maxSpeed);
 
-        transform.Translate(velocity * Time.deltaTime);
+        transform.Translate(velocity);
     }
 
     public void DrawCircle(float radius, int circlePoints){
@@ -73,6 +90,7 @@ public class Player : MonoBehaviour
             Vector3 c2 = new Vector3(Mathf.Cos((Mathf.PI * 2 / circlePoints) * (i+1))  * radius, Mathf.Sin((Mathf.PI * 2 / circlePoints) * (i+1)) * radius);
             Debug.DrawLine(transform.position + c1, transform.position + c2, circleColor);
         }
+
     }
 
     public void SpawnPowerups(float radius, int circlePoints){
